ebd487b5947acbb728b356c4c7733af0f797d474
Simon Köhler <info@simon-koehler.com>
2020-07-19 12:00:44 -0500

    Moved popovers to Root.js from Form.js

--- Resources/Public/Js/Root.js
+++ Resources/Public/Js/Root.js
@@ -96,4 +96,12 @@ document.addEventListener("DOMContentLoaded", function() {
         autoResizeTextfields(el);
     });
 
+    /*
+    * Initializing Bootstrap popovers
+    */
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
 });

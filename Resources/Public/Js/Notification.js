04e12254ce49072ada0f65fcdb3db2700543954b
Simon Köhler <info@simon-koehler.com>
2020-07-21 08:53:53 -0500

    disabled demo code

--- Resources/Public/Js/Notification.js
+++ Resources/Public/Js/Notification.js
@@ -10,12 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     if (Notification.permission === "granted") {
+        /*
         let options = {
             title: 'Thank you',
             body: 'Permission has been granted. We will send you some notifications from time to time!',
             url: 'https://opensourceproject.com'
         };
         showNotification(options);
+        */
     }
     else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(permission => {

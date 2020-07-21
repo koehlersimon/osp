15442e1d76459e4d38e165ebad905af13b35e6c4
Simon Köhler <info@simon-koehler.com>
2020-07-21 09:58:08 -0500

    new page TS config file added

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,6 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'osp',
+    'Configuration/TypoScript/page.ts',
+    'Osp - Content and Page Configuration'
+);
---
+++ Configuration/TypoScript/page.ts
@@ -0,0 +1,12 @@
+TCEFORM{
+    tt_content{
+        frame_class {
+            disabled = 0
+            removeItems = ruler-before, ruler-after, indent, indent-left, indent-right
+            addItems.card = OSP card
+        }
+        layout {
+            disabled = 1
+        }
+    }
+}

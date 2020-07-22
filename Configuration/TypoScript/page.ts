affb8063dd19e60fd5c841d368598847d5ed32ab
Simon Köhler <info@simon-koehler.com>
2020-07-22 11:46:37 -0500

    new CE wizards

--- Configuration/TypoScript/page.ts
+++ Configuration/TypoScript/page.ts
@@ -10,3 +10,39 @@ TCEFORM{
         }
     }
 }
+
+mod.wizards {
+    newContentElement.wizardItems {
+        plugins {
+            elements {
+                osp_home {
+                    iconIdentifier = osp-home
+                    title = OSP Home
+                    description = List of external and friends posts
+                    tt_content_defValues {
+                        CType = list
+                        list_type = osp_home
+                    }
+                }
+                osp_wall {
+                    iconIdentifier = osp-wall
+                    title = OSP Wall
+                    description = List of your own private posts
+                    tt_content_defValues {
+                        CType = list
+                        list_type = osp_wall
+                    }
+                }
+                osp_form {
+                    iconIdentifier = osp-form
+                    title = OSP Post Form
+                    description = The form to create a post
+                    tt_content_defValues {
+                        CType = list
+                        list_type = osp_form
+                    }
+                }
+            }
+        }
+    }
+}

da434580c80b1d5e2f85e0334311ae0bd12ceba2
Simon Köhler <info@simon-koehler.com>
2020-07-22 11:47:31 -0500

    backend layouts added

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,5 +2,11 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'osp',
     'Configuration/TypoScript/page.ts',
-    'Osp - Content and Page Configuration'
+    'Osp - Content & Page Configuration'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'osp',
+    'Configuration/TypoScript/belayouts.typoscript',
+    'Osp - Page Layouts'
 );

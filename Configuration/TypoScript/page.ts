82265a96202da5c5f401c4e94def1b2649b67780
Simon Köhler <info@simon-koehler.com>
2020-07-28 09:10:00 -0500

    code improvements

--- Configuration/TypoScript/page.ts
+++ Configuration/TypoScript/page.ts
@@ -14,53 +14,54 @@ TCEFORM{
     }
 }
 
-mod.wizards {
-    newContentElement.wizardItems {
-        plugins {
-            elements {
-                osp_list {
-                    iconIdentifier = osp-home
-                    title = OSP List
-                    description = Displays a list of posts with various options
-                    tt_content_defValues {
-                        CType = list
-                        list_type = osp_list
+mod{
+    web_layout.tt_content.preview.osp_menu = EXT:osp/Resources/Private/Backend/Previews/OspMenu.html
+    wizards {
+        newContentElement.wizardItems {
+            plugins {
+                elements {
+                    osp_list {
+                        iconIdentifier = osp-home
+                        title = OSP List
+                        description = Displays a list of posts with various options
+                        tt_content_defValues {
+                            CType = list
+                            list_type = osp_list
+                        }
                     }
-                }
-                osp_form {
-                    iconIdentifier = osp-form
-                    title = OSP Post Form
-                    description = The form to create a post
-                    tt_content_defValues {
-                        CType = list
-                        list_type = osp_form
+                    osp_form {
+                        iconIdentifier = osp-form
+                        title = OSP Post Form
+                        description = The form to create a post
+                        tt_content_defValues {
+                            CType = list
+                            list_type = osp_form
+                        }
                     }
-                }
-                osp_post_detail {
-                    iconIdentifier = osp-form
-                    title = OSP Post Detail
-                    description = The Post detail view
-                    tt_content_defValues {
-                        CType = list
-                        list_type = osp_post_detail
-                    }
-                }
-            }
-        }
-        menu{
-            elements{
-                osp_menu {
-                    iconIdentifier = osp-menu
-                    title = OSP Menu
-                    description = The form to create a post
-                    tt_content_defValues {
-                        CType = osp_menu
+                    osp_post_detail {
+                        iconIdentifier = osp-form
+                        title = OSP Post Detail
+                        description = The Post detail view
+                        tt_content_defValues {
+                            CType = list
+                            list_type = osp_post_detail
+                        }
                     }
                 }
             }
-            show := addToList(osp_menu)
+            menu{
+                elements{
+                    osp_menu {
+                        iconIdentifier = osp-menu
+                        title = OSP Menu
+                        description = The form to create a post
+                        tt_content_defValues {
+                            CType = osp_menu
+                        }
+                    }
+                }
+                show := addToList(osp_menu)
+            }
         }
     }
 }
-
-web_layout.tt_content.preview.osp_menu = EXT:osp/Resources/Private/Backend/Previews/OspMenu.html
